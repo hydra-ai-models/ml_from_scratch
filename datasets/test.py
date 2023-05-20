8cbb253dd54f773e65ad65396524728bd4c5ed36
Jay <jay@Jays-MacBook-Pro.local>
2023-05-20 12:52:01 -0700

    Fixing dataset tests.

--- datasets/test.py
+++ datasets/test.py
@@ -6,8 +6,10 @@
 '''
 
 from datasets.text_dataset import TextDataset
+from tokenizers.base_tokenizer import BaseTokenizer
 from tokenizers.char_tokenizer import CharTokenizer
-from torch import Tensor
+from torch import Tensor, equal
+from typing import Type
 
 import unittest
 
@@ -17,15 +19,21 @@ class CharTokenizerTester(unittest.TestCase):
         document = 'Hello, how are you?'
         tokenizer = CharTokenizer(document)
 
-        self.test_at_indices([0, 2], document, tokenizer)
+        self.generate_items_and_test([0, 2], document, tokenizer)
 
     def compare_items(self, item1: dict[str, Tensor], item2: dict[str, Tensor]) -> bool:
-        item1_list = list(item1)
-        item2_list = list(item2)
-        if len(item1_list) != len(item2_list):
+        item1_keys = sorted(item1.keys())
+        item2_keys = sorted(item2.keys())
+        if len(item1_keys) != len(item2_keys):
             return False
 
-    def test_at_indices(self, indices_to_test, document, tokenizer):
+        for (item1_key, item2_key) in zip(item1_keys, item2_keys):
+            if (item1_key != item2_key) or not (equal(item1[item1_key], item2[item2_key])):
+                return False
+        return True
+
+
+    def generate_items_and_test(self, indices_to_test: list[int], document: str, tokenizer: Type[BaseTokenizer]):
         max_block_size = 8
         dataset = TextDataset(max_block_size = max_block_size, tokenizer = tokenizer, document = document)
 
@@ -35,7 +43,7 @@ class CharTokenizerTester(unittest.TestCase):
                 'features': Tensor(tokenizer.encode(document[index : index + max_block_size])),
                 'labels': Tensor(tokenizer.encode(document[index + 1 : index + max_block_size + 1]))
             }
-            self.assertEqual(self.compare_items(expected_item, actual_item))
+            self.assertTrue(self.compare_items(expected_item, actual_item), f'{expected_item=} not matching {actual_item=}')
 
 
 
